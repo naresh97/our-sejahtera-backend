01d1090975bbc8cc8ef421f05c2e4c0cc393eb64
Nareshkumar Rao <contact@nareshkumarrao.com>
2021-07-29 22:40:41 +0200

    ran linter

--- src/routes/TelegramWebhookRoute.js
+++ src/routes/TelegramWebhookRoute.js
@@ -4,59 +4,66 @@ const { strings_en } = require("../strings");
 const { sendTelegramMessage } = require("../telegram");
 
 function TelegramWebhookRoute(req, res) {
-
-    try{
-        if(req.body.message.connected_website){
-            sendTelegramMessage(req.body.message.from.id, "Thanks for using OurSejahtera! Let's stay safer together <3");
-        }else{
-            const messageText = req.body.message.text;
-            const telegramID = req.body.message.from.id;
-            if (messageText.toLowerCase() == "/covidpositive") {
-                userInfected(telegramID, (result) => {
-                    if(result.saved){
-                        sendTelegramMessage(telegramID, strings_en.telegram_inform_positive);
-                        informContacts(telegramID);
-                    }else{
-                        sendTelegramMessage(telegramID, "Sorry, something went wrong.");
-                    }
-                });
-            }
-        }
-    }catch(e){
-        console.log("Could not get Telegram Message");
+  try {
+    if (req.body.message.connected_website) {
+      sendTelegramMessage(
+        req.body.message.from.id,
+        "Thanks for using OurSejahtera! Let's stay safer together <3"
+      );
+    } else {
+      const messageText = req.body.message.text;
+      const telegramID = req.body.message.from.id;
+      if (messageText.toLowerCase() == "/covidpositive") {
+        userInfected(telegramID, (result) => {
+          if (result.saved) {
+            sendTelegramMessage(
+              telegramID,
+              strings_en.telegram_inform_positive
+            );
+            informContacts(telegramID);
+          } else {
+            sendTelegramMessage(telegramID, "Sorry, something went wrong.");
+          }
+        });
+      }
     }
+  } catch (e) {
+    console.log("Could not get Telegram Message");
+  }
 
   res.send();
 }
 
-function informContacts(telegramID, doneCallback=()=>{}){
-    User.findOne({
+function informContacts(telegramID, doneCallback = () => {}) {
+  User.findOne({
+    where: {
+      telegram: telegramID,
+    },
+  }).then((user) => {
+    if (user) {
+      const userRowID = user.id;
+      Contact.findAll({
         where: {
-            telegram: telegramID,
-        }
-    }).then(user => {
-        if(user){
-            const userRowID = user.id;
-            Contact.findAll({
-                where: {
-                    [Op.or]: [{user: userRowID}, {with: userRowID}],
-                }
-            })
-            .then(result => {
-                result.forEach(contact => {
-                    const otherPersonID = contact.user == userRowID ? contact.with : contact.user;
-                    User.findOne({
-                        where: {
-                            id: otherPersonID,
-                        }
-                    }).then(otherPerson => {
-                        sendTelegramMessage(otherPerson.telegram, strings_en.telegram_inform_infect);
-                    });
-                });
-            });
-        }
-    });
-
+          [Op.or]: [{ user: userRowID }, { with: userRowID }],
+        },
+      }).then((result) => {
+        result.forEach((contact) => {
+          const otherPersonID =
+            contact.user == userRowID ? contact.with : contact.user;
+          User.findOne({
+            where: {
+              id: otherPersonID,
+            },
+          }).then((otherPerson) => {
+            sendTelegramMessage(
+              otherPerson.telegram,
+              strings_en.telegram_inform_infect
+            );
+          });
+        });
+      });
+    }
+  });
 }
 
 function userInfected(telegramID, doneCallback) {
